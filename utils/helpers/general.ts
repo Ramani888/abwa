50316feb88e1c900e806534e583ee4937d67c925
Ramani888 <ramanidivyesh888@gmail.com>
2025-07-25 08:06:35 +0530

    supplier payment section in field in apply indian number formate

--- utils/helpers/general.ts
+++ utils/helpers/general.ts
@@ -13,3 +13,12 @@ export const formatCurrency = (amount: number, includeSymbol = true, isFixedPoin
 
   return new Intl.NumberFormat('en-IN', options).format(amount);
 };
+
+export const formatIndianNumber = (num: string | number) => {
+  const x = num?.toString().replace(/,/g, "");
+  const lastThree = x?.substring(x.length - 3);
+  const otherNumbers = x?.substring(0, x.length - 3);
+  return otherNumbers !== ""
+    ? otherNumbers.replace(/\B(?=(\d{2})+(?!\d))/g, ",") + "," + lastThree
+    : lastThree;
+}
